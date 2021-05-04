756f6224d0b18403b5a0eb939556e9fbdd3b1d99
Hamada Ragab <hamadaragab550@gmail.com>
2021-05-04 12:13:26 +0200

    last2

--- testss/ViewController.swift
+++ testss/ViewController.swift
@@ -11,7 +11,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("hamada")
+        print("last commit from main")
         // Do any additional setup after loading the view.
     }
 
